18525adafaa98105e845cebc5518b433d50eeb4b
Johan <johan@rattgrafiska.se>
2025-02-10 16:15:46 +0100

    Update index.php
    Versionbump

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Rätt Grafiska Git Updater
 Description: Hanterar automatiska uppdateringar för Ratt Grafiskas plugins via GitHub.
-Version: 1.0.1
+Version: 1.0.2
 Author: Ratt Grafiska
 Plugin URI: https://github.com/Ratt-Grafiska/rg-git-updater
 Update URI: https://github.com/Ratt-Grafiska/rg-git-updater
